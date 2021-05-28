b6b273d6b445f3b64c9af0a015614f8d5209697d
Yon Ploj <yon.ploj@gmail.com>
2021-05-28 16:56:01 +0200

    fixed donut generation

--- donut.c
+++ donut.c
@@ -38,14 +38,21 @@ void printMatrix(Matrix m) {
 }
 
 Matrix makeMatrix(int h, int w) {
-	Matrix* m = malloc(sizeof(Matrix));
-	m->w = w;
-	m->h = h;
-	m->data = malloc(w * sizeof(double*));
+	Matrix m;
+	m.w = w;
+	m.h = h;
+	m.data = malloc(w * sizeof(double*));
 	for (int i = 0; i < w; i++) {
-		m->data[i] = malloc(h * sizeof(double));
+		m.data[i] = malloc(h * sizeof(double));
 	}
-	return *m;
+	return m;
+}
+
+void destroyMatrix(Matrix* m) {
+	for (int i = 0; i < m->w; i++) {
+		free(m->data[i]);
+	}
+	free(m->data);
 }
 
 Matrix rotation3d(char axis, double angle) {
@@ -88,6 +95,14 @@ Matrix rotation3d(char axis, double angle) {
 	return m;
 }
 
+int dot(Vector a, Vector b) {
+	int sum = 0;
+	for (int i = 0; i < a.dim; i++) {
+		sum += a.data[i] * b.data[i];
+	}
+	return sum;
+}
+
 void transform(Vector* p, Matrix m) {
 	// p->dim == m.h
 	Vector res = { .dim = p->dim, .data = malloc(p->dim * sizeof(double)) };
@@ -105,33 +120,21 @@ void transform(Vector* p, Matrix m) {
 
 char getSymbol(int distance) {
 	distance /= 3;
-	distance += 3;
+	distance += 5;
 	if (distance < 0) return 'X';
 	switch (distance) {
-		case 0: return '#';
-		case 1: return '@';
-		case 2: return '$';
-		case 3: return '%';
-		case 4: return '*';
-		case 5: return '~';
-		case 6: return ':';
-		case 7: return '.';
-		default: return ' ';
-	}
-
-	switch (distance) {
-		case 0: return '#';
-		case 1: return '@';
-		case 2: return '&';
-		case 3: return '%';
-		case 4: return '$';
+		case 0: return '@';
+		case 1: return '$';
+		case 2: return '#';
+		case 3: return '*';
+		case 4: return '!';
 		case 5: return '=';
-		case 6: return '*';
-		case 7: return '~';
-		case 8: return ';';
-		case 9: return ':';
-		case 10: return '.';
-		case 11: return '`';
+		case 6: return ';';
+		case 7: return ':';
+		case 8: return '~';
+		case 9: return '-';
+		case 10: return ',';
+		case 11: return '.';
 		default: return ' ';
 	}
 }
@@ -139,84 +142,57 @@ char getSymbol(int distance) {
 void show(Vector* points, int count) {
 	for (int y = -SCREEN_HEIGHT; y < SCREEN_HEIGHT; y++) {
 		for (int x = -SCREEN_WIDTH; x < SCREEN_WIDTH; x++) {
-			int closest = 1000000; // probably won't have a depth more than this
+			// find the closest point whose coordinates round to (x, y)
+			int closest_index = -1;
 			for (int k = 0; k < count; k++) {
 				if (round(points[k].data[0]) == x && round(points[k].data[1]) == y) {
-					int dist = round(points[k].data[2]);
-					if (closest > dist) closest = dist;
+					if (closest_index == -1) closest_index = k;
+					int depth = round(points[k].data[2]);
+					int min_depth = round(points[closest_index].data[2]);
+					if (depth < min_depth) closest_index = k;
 				}
 			}
-			printf("%c", getSymbol(closest));
+			if (closest_index != -1) {
+				//int luminance = dot(normals[closest_index], lightSource);
+				//printf("%c", getSymbol(luminance));
+				if (round(points[closest_index].data[0]) != x) printf("X doesnt match\n");
+				if (round(points[closest_index].data[1]) != y) printf("Y doesnt match\n");
+				printf("%c", getSymbol(round(points[closest_index].data[2])));
+			}
+			else printf(" ");
 		}
 		printf("\n");
 	}
 }
 
 int main(int argc, char** argv) {
-	// DEMO: basic debug stuffs
-	/*
-	Vector a = { .dim = 3, .data = malloc(3*sizeof(int)) };
-	Matrix m = makeMatrix(3, 3);
-	printVector(a);
-	printMatrix(m);
-	Matrix project = makeMatrix(3, 2);
-	Matrix r = rotation3d('y', PI/2);
-	Vector a = { .dim = 3, .data = malloc(3*sizeof(int)) };
-	a.data[0] = 0;
-	a.data[1] = 1;
-	a.data[2] = 0;
-	transform(&a, r);
-	printVector(a);
-	return 0;
-	*/
-
-	// DEMO: spinny lines and shit
-	/*
-	const int POINT_COUNT = 20;
-	Vector points[POINT_COUNT];
-	for (int i = 0; i < POINT_COUNT; i++) {
-		Vector p = { .dim = 3, .data = malloc(3*sizeof(int)) };
-		points[i] = p;
-	}
-	double t = 0;
-	while (true) {
-		Matrix rotation = rotation3d('x', t);
-		for (int i = 0; i < POINT_COUNT/2; i++) {
-			points[i].data[0] = i;
-			points[i].data[1] = POINT_COUNT/2-1;
-			points[i].data[2] = i;
-			transform(&points[i], rotation);
-		}
-		Matrix rotation2 = rotation3d('z', t);
-		for (int i = POINT_COUNT/2; i < POINT_COUNT; i++) {
-			points[i].data[0] = i - POINT_COUNT/2;
-			points[i].data[1] = 0;
-			points[i].data[2] = 0;
-			transform(&points[i], rotation2);
-		}
-
-		show(points, POINT_COUNT);
-		printf("time is %f. sin(t) = %f, cos(t) = %f\n", t, sin(t), cos(t));
-		usleep(200*1000);
-		t += 0.1;
-	}
-	*/
+	Vector lightSource = { .dim = 3, .data = malloc(3*sizeof(int)) };
+	lightSource.data[0] = -SCREEN_WIDTH;
+	lightSource.data[1] = -SCREEN_HEIGHT;
+	lightSource.data[2] = 0;
 
 	Matrix rotationX = rotation3d('x', 0.1);
 	Matrix rotationY = rotation3d('y', 0.1);
 	Matrix rotationZ = rotation3d('z', 0.1);
 
-	const int INNER_POINTS = 40;
-	const int OUTER_POINTS = 30;
-	const int inner_radius = 10;
+	const int INNER_POINTS = 50;
+	const int OUTER_POINTS = 50;
+	const int POINTS = INNER_POINTS * OUTER_POINTS;
+	const int inner_radius = 6;
 	const int outer_radius = 3;
+
 	Vector inner[INNER_POINTS];
-	Vector outer[OUTER_POINTS * INNER_POINTS];
+	Vector outer[POINTS * 2];
+	//Vector* normals = &outer[POINTS];
+	// append normals array at the end of the outer array,
+	// so I only have to call transform() once
+
 	for (int i = 0; i < INNER_POINTS; i++) {
+		Matrix rotate = rotation3d('z', i * TWOPI/INNER_POINTS);
 		Vector p = { .dim = 3, .data = malloc(3*sizeof(int)) };
 		inner[i] = p;
-		inner[i].data[0] = cos(i * TWOPI/INNER_POINTS)*inner_radius;
-		inner[i].data[1] = sin(i * TWOPI/INNER_POINTS)*inner_radius;
+		inner[i].data[0] = inner_radius;
+		inner[i].data[1] = inner_radius;
 		inner[i].data[2] = 0;
 		for (int j = 0; j < OUTER_POINTS; j++) {
 			int offset = OUTER_POINTS * i + j;
@@ -229,18 +205,25 @@ int main(int argc, char** argv) {
 			outer[offset].data[0] += inner[i].data[0];
 			outer[offset].data[1] += inner[i].data[1];
 			outer[offset].data[2] += inner[i].data[2];
+			transform(&outer[offset], rotate);
+
+			//Vector s = { .dim = 3, .data = malloc(3*sizeof(int)) };
+			//normals[offset] = s;
+			//normals[offset].data[0] = outer[offset].data[0] - inner[i].data[0];
+			//normals[offset].data[1] = outer[offset].data[1] - inner[i].data[1];
+			//normals[offset].data[2] = outer[offset].data[2] - inner[i].data[2];
 		}
+		transform(&inner[i], rotate);
+		destroyMatrix(&rotate);
 	}
 
 	while (true) {
-		//show(inner, INNER_POINTS);
-		show(outer, OUTER_POINTS * INNER_POINTS);
-		for (int i = 0; i < INNER_POINTS * OUTER_POINTS; i++) {
+		show(outer, POINTS);
+		for (int i = 0; i < POINTS; i++) {
 			transform(&outer[i], rotationX);
 			transform(&outer[i], rotationY);
 		}
 		usleep(100000);
 	}
 	return 0;
-	//*/
 }
